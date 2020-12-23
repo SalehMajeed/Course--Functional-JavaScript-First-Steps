4d82cb594d2748b1ac61fe0f27efaf3d08197067
= <saleh.majeed8@gmail.com>
2020-12-23 11:20:47 +0530

    map

--- js/part4.js
+++ js/part4.js
@@ -1,27 +1,45 @@
-function filter(predicate_function, array) {
+// function filter(predicate_function, array) {
+//     if (length(array) == 0) return [];
+//     const first_item = head(array);
+//     const filtered_first = predicate_function(first_item) ? [first_item] : [];
+//     return concat(filtered_first, filter(predicate_function, tail(array)));
+// }
+
+// function length(array) {
+//     return array.length;
+// }
+
+// function head(array) {
+//     return array[0];
+// }
+
+// function tail(array) {
+//     return array.slice(1);
+// }
+
+// function concat(array1, array2) {
+//     return array1.concat(array2);
+// }
+
+// function is_even(value) {
+//     return value % 2 == 0;
+// }
+// console.log(filter(is_even, [1, 2, 3, 4, 5]));
+
+function map(fn, array) {
     if (length(array) == 0) return [];
-    const first_item = head(array);
-    const filtered_first = predicate_function(first_item) ? [first_item] : [];
-    return concat(filtered_first, filter(predicate_function, tail(array)));
+    return [fn(head(array))].concat(map(fn, tail(array)));
 }
-
-function length(array) {
-    return array.length;
-}
-
-function head(array) {
-    return array[0];
-}
-
 function tail(array) {
     return array.slice(1);
 }
-
-function concat(array1, array2) {
-    return array1.concat(array2);
+function head(array) {
+    return array[0];
 }
-
-function is_even(value) {
-    return value % 2 == 0;
+function length(array) {
+    return array.length;
 }
-console.log(filter(is_even, [1, 2, 3, 4, 5]));
+function fn(n) {
+    return n * 2;
+}
+console.log(map(fn, [1, 2, 3, 4, 5]));
